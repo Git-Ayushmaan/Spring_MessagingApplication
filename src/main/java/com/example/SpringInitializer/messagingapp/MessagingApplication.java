f22327a61fbb59832311f512500efd54bc7ff658
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-02-21 18:51:04 +0530

    [Ayushmaan Tiwari] Add. Fetching name from request parameters

--- src/main/java/com/example/SpringInitializer/messagingapp/MessagingApplication.java
+++ src/main/java/com/example/SpringInitializer/messagingapp/MessagingApplication.java
@@ -1,12 +1,19 @@
 package com.example.SpringInitializer.messagingapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessagingApplication {
     @GetMapping("/hello")
-    public String sayHello(){
+    public String sayHello() {
         return "Hello From Bridgelabz";
     }
+    @GetMapping("/hello/query")
+    public String fetchName(@RequestParam String name){
+        return "Hello "+name;
+    }
+
 }
+
